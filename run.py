4bd41b7fc0e92f7b18124053252071e4142a93fd
Lydia Young <lydsjoyy@gmail.com>
2023-07-07 18:49:16 +0000

    Fix indentation errors

--- run.py
+++ run.py
@@ -18,28 +18,33 @@ def walk():
 def first_location():
   print('Amidst the radiant sunbeams that bathed the vibrant medieval fairgrounds of MerryMeadow, the joyful atmosphere of the fair seemed unbreakable.\nColorful pennants fluttered merrily in the breeze, mingling with the tantalizing aroma of caramelized apples and freshly spun cotton candy.\n Laughter and music filled the air, and chatter echoed across the bustling fair. But hidden beneath the facade of delight, a dark shadow had cast its spell over the festivities.\n Lady Amara, beloved by all the folk of Merrymeadow, had been discovered dead within her regal tent, her crown askew and a haunting mystery in the air.')
   print('Are you ready to uncover the truth of Merrymeadow?\n')
-  answer = str(input('Please answer yes or no: \n'))
+  answer = input('Please answer yes or no: \n')
 
-  if answer = "yes" or "Yes" : 
-  print('Lady Amara was discovered by her daughter in law Clara.\n Go find Clara?')
+  if answer == "yes": 
+       print('Lady Amara was discovered by her daughter in law Clara.\n Go find Clara?')
        find = str(input())
-       if find = "yes" or "Yes"
-       findclara()
+       if find == "yes":
+            find_clara()
 
-  elif answer = "no" or "No" : 
-  print('Not up for the challenge?... heading back home.')
-  print("Type head back to town to try again.\n")
-      restart = str(input())
-       if restart = "head back to town" or "Head back"
-       main()
-       
-  except : ValueError :
-  print("Sorry I didn't catch that...")
-  break
+  else:
+       print('Not up for the challenge?... heading back home.')
+       input = ('Would you like to head back to town? \n')
+       restart = str(input())
+       if restart == "yes":
+                main()
+       if restart == "no":
+               print('Game Over.')
+
+# def find_clara():
+
+# def find_the_major():
+
+# def find_Count_Crispin():
 
 def main():
-     game_start()
-     walk()
-     first_location()
+      game_start()
+      walk()
+      first_location()
+      findclara()
 
 main()
