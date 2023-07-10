7e8d7e26471eaf9f875e652c86b9d1bf4dcc9265
Lydia Young <lydsjoyy@gmail.com>
2023-07-10 18:17:48 +0000

    Try and fix code errors

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
 ## Project Name : Merrymeadow Mystrey
 
-# **TABLE OF Contents**
-
 * [**Project Brief**](#Project-Brief)
+My project is a murder mystrey game built in Python. The game is set in the town of Merry Meadow and the murder takes place during the towns funfair to Lady Amara. In this game you inbark on a quest to find out who murdered lady Amara. 
+
+# **TABLE OF Contents**
 
 * [**User Experience** ](#User-experienceX)
 * [**User Stories**](#User-Stories)
@@ -17,12 +17,16 @@
 * [**Credits** ](#Credits)
 * [**Acknowledgements** ](#Acknowledgements)
 
-# Project-Brief 
-
 # User-Experience 
 Project Outcomes:
 
+To build a game in python and deploy it to Heroku. 
+To show my python skills and understanding in creating this game. 
+
 # User-Stories:
+I want to play a line terminal game that's fun and engaging and has some user interation. I want it to allow you to think and make choices.
+
+I want to play a simple murder mystrey game that gives me a chance to guess the characters.
 
 # Features 
 
--- run.py
+++ run.py
@@ -1,40 +1,41 @@
-# Game is set in a funfair. Player gives their tickets to enter
-def walk()
-def first_location()
-def findclara()
+# Game is set in a funfair. Player gives their tickets to enter. 
+# There are three suspects in this game
+suspects = ["Clara", "The Major", "Count Chrispin"]
+locations = []
 
 def game_start():
- while True:
       print('"Tickets please... Tickets.. Hi, thank you..Oh! Hello, you must be new!\n Welcome to Merrymeadows funfair. My name is Lucy. What is your name?"\n')
       name = input('Please Enter your name:\n ')
       print('"Hi,' + name ,'"')
-      location = input('Where would you like to go first? The lace market, Merry-go-round, or the farm shop? \n')
-      print('"Great! Lets go!"\n')
-      print('Heading to ' + location)
+      print('Where would you like to go first?',"The Lace market, The Merry-go-round, or The farm shop")
+      travel = input()   
+      print('"Great! Lets go!"')
+      print('Heading to ' + travel)
       walk()
 
- walk():
+def walk():
       print('...\n')
-      print('"STOP! DO NOT GO ANY FURTHER"\n "Sorry, we are now closing...\n')
+      print('"STOP! DO NOT GO ANY FURTHER"', '"Sorry, we are now closing..."')
       first_location()
 
-first_location():
-       print('Amidst the radiant sunbeams that bathed the vibrant medieval fairgrounds of MerryMeadow, the joyful atmosphere of the fair seemed unbreakable.\nColorful pennants fluttered merrily in the breeze, mingling with the tantalizing aroma of caramelized apples and freshly spun cotton candy.\n Laughter and music filled the air, and chatter echoed across the bustling fair. But hidden beneath the facade of delight, a dark shadow had cast its spell over the festivities.\n Lady Amara, beloved by all the folk of Merrymeadow, had been discovered dead within her regal tent, her crown askew and a haunting mystery in the air.')
-       print('Are you ready to uncover the truth of Merrymeadow?\n')
-       answer = input('Please answer yes or no: \n')
-       if answer == "yes":
-            print('Lady Amara was discovered by her daughter in law Clara.\n Go find Clara?')
-            find = str(input())
-            if find == "yes":
-                find_clara()
-           elif find == "no":
-                print('Not up for the challenge?... heading back home.')
-                input = ('Would you like to head back to town? \n')
-                restart = str(input())
-                     if restart == "yes":
-                           main()
-                     if restart == "no":
-                           print('Game Over.')
+def first_location():
+     print =('Amidst the radiant sunbeams that bathed the vibrant medieval fairgrounds of MerryMeadow, the joyful atmosphere of the fair seemed unbreakable.\nColorful pennants fluttered merrily in the breeze, mingling with the tantalizing aroma of caramelized apples and freshly spun cotton candy. Laughter and music filled the air, and chatter echoed across the bustling fair. But hidden beneath the facade of delight, a dark shadow had cast its spell over the festivities. Lady Amara, beloved by all the folk of Merrymeadow, had been discovered dead within her regal tent, her crown askew and a haunting mystery in the air.')
+     input = ('Are you ready to uncover the truth of Merrymeadow? Yes or No?')
+     input = yes
+     while True: 
+          print('Lady Amara was discovered by her daughter in law Clara.')
+          print = input('Find Clara?')
+          input = yes
+          while True :
+               find_clara()
+     else:
+          print('Not up for the challenge?... heading back home.')
+          input = ('Would you like to head back to town? \n')
+          input = yes
+          if answer == yes :
+               main()
+          if answer == no :
+               print('Game Over.')              
 
 # def find_clara():
 
@@ -46,6 +47,8 @@ def main():
       game_start()
       walk()
       first_location()
-      findclara()
+     # findclara()
+     # find_the_major()
+     # find_count_crispin()
 
 main()
