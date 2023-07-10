d23d196e54bfc73dc37d867f307244f1003025d5
Lydia Young <lydsjoyy@gmail.com>
2023-07-10 16:35:44 +0000

    Define functions at the top of the code

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 # Game is set in a funfair. Player gives their tickets to enter
+def walk()
+def first_location()
+def findclara()
+
 def game_start():
  while True:
       print('"Tickets please... Tickets.. Hi, thank you..Oh! Hello, you must be new!\n Welcome to Merrymeadows funfair. My name is Lucy. What is your name?"\n')
@@ -9,30 +13,28 @@ def game_start():
       print('Heading to ' + location)
       walk()
 
-def walk():
+ walk():
       print('...\n')
       print('"STOP! DO NOT GO ANY FURTHER"\n "Sorry, we are now closing...\n')
       first_location()
 
-def first_location():
+first_location():
        print('Amidst the radiant sunbeams that bathed the vibrant medieval fairgrounds of MerryMeadow, the joyful atmosphere of the fair seemed unbreakable.\nColorful pennants fluttered merrily in the breeze, mingling with the tantalizing aroma of caramelized apples and freshly spun cotton candy.\n Laughter and music filled the air, and chatter echoed across the bustling fair. But hidden beneath the facade of delight, a dark shadow had cast its spell over the festivities.\n Lady Amara, beloved by all the folk of Merrymeadow, had been discovered dead within her regal tent, her crown askew and a haunting mystery in the air.')
        print('Are you ready to uncover the truth of Merrymeadow?\n')
        answer = input('Please answer yes or no: \n')
-
-  if answer == "yes": 
-       print('Lady Amara was discovered by her daughter in law Clara.\n Go find Clara?')
-       find = str(input())
-       if find == "yes":
-            find_clara()
-
-  else:
-       print('Not up for the challenge?... heading back home.')
-       input = ('Would you like to head back to town? \n')
-       restart = str(input())
-       if restart == "yes":
-                main()
-       if restart == "no":
-               print('Game Over.')
+       if answer == "yes":
+            print('Lady Amara was discovered by her daughter in law Clara.\n Go find Clara?')
+            find = str(input())
+            if find == "yes":
+                find_clara()
+           elif find == "no":
+                print('Not up for the challenge?... heading back home.')
+                input = ('Would you like to head back to town? \n')
+                restart = str(input())
+                     if restart == "yes":
+                           main()
+                     if restart == "no":
+                           print('Game Over.')
 
 # def find_clara():
 
